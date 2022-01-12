f96c53977a1a449c38d6fcba2b15f60e5db223fb
Hugo LAVEAU <46439381+hyugzz@users.noreply.github.com>
2022-01-12 19:52:31 +0100

    Tweaked peak calculation (may be subject to changes) Corrected data comparison Added windowing
    Tests are still to run

--- main.jl
+++ main.jl
@@ -13,8 +13,8 @@ a = signal_utils.cofdm([[true, false, true, true, false, true, false, false],
                         [true, true, false, true, false, true, false, true],
                         [false, true, false, false, false, true, false, true],
                         [false, false, true, false, true, false, true, true]],
-                        [[false,true], [true,true], [false, true], [false, false]],
-                        8, [1,8], 15000.0, 5.00e6, 0.001, 4, 1.5e7)
+                        [[false,true, true], [true,true, false], [true, false, true], [true, false, false]],
+                        8, [1,8, 11], 15000.0, 5.00e6, 0.001, 2, 1.5e7)
 tles = read_tle("starlink.txt")
 doppler_ratio = Float64[]
 
@@ -34,91 +34,20 @@ for i in 1:length(x)
     MyGravity.set_pos(sender, [x[i], y[i], z[i]])
     MyGravity.set_spe(sender, [x1[i], y1[i], z1[i]])
 end
-plot(doppler_ratio)
 
 #Generating signal
-sig = signal_utils.generate_signal(a, 1.0)
-Plots.plot(sig)
-
-signal_utils.AnalyzeSignal(a, sig, 4)
-
-plots = Plot[]
-fft_data = Vector{Array{Float64}}
-
+sig = signal_utils.generate_signal(a, 0.85)
 #DFT transform of the signal
 plots, fft_data, fft_freq = signal_utils.AnalyzeSignal(a, sig, 4)
-for i in 1:length(plots)
-    display(plots[i])
-end
-plot(fft_freq, fft_data[1])
 
-k = [Vector{Bool}() for _ in 1:length(a.data)]
-for i in 1:4
-    k[i] = signal_utils.reconstruct_data(a, fft_data[i], fft_freq)
-end
-
-
-shifted_ffts = [Vector{Bool}()  for _ in 1:length(fft_data)]
-bits = Float64[]
-for i in 1:length(fft_data)
-    shifted_fft = Vector{Float64}()
-    for k in 1:length(fft_data[i])    # FFT frequency shift loop
-        if trunc(Int,k*0.85) < length(fft_data[i])
-            if trunc(Int, k*0.85) == 0
-                push!(shifted_fft, fft_data[i][1])
-            else
-                push!(shifted_fft, fft_data[i][trunc(Int, k*0.85)])
-            end
-        else
-            push!(shifted_fft, 0.0)
-        end
-    end
-    shifted_ffts[i] = signal_utils.reconstruct_data(a, shifted_fft, fft_freq)
-end
-popfirst!(shifted_fft)
 # Is the is shift correct for this bit
-bits = signal_utils.reconstruct_data(a, shifted_fft, fft_freq)
-
-plot(fft_freq,shifted_fft)
-
-# Stats about orbit
-length([fft_data[1]])
-for i in 1:151
-    append!(dist, abs(signal_utils.NormeVecteur(sender.p - receiver.p)))
-    append!(doppler_ratio, signal_utils.Doppler(1.0, sender, receiver))
-    Main.signal_utils.generate_signal(sig, doppler_ratio)
-    Main.MyGravity.set_pos(sender, [x[i], y[i], z[i]])
-    Main.MyGravity.set_spe(sender, [x1[i], y1[i], z1[i]])
-end
-
-shifted_fft = Float64[]
-tmp_fft = fft_data[begin:end][trunc(Int, length(fft_data[begin:end])/2):length(fft_data[begin:end])]
-true_shift, k = signal_utils.ShiftSignal(fft_data, a, fft_freq)
-for i in 2:trunc(Int, length(fft_data[1])/2)
-    if i < length(tmp_fft)/2
-        push!(shifted_fft, tmp_fft[trunc(Int, i*1/(0.5))])
+for i in 1:4
+    bits = signal_utils.reconstruct_data(a, signal_utils.SmoothedZscoreAlgo(fft_data[i], 3, 4, 0.5)[1], fft_freq)
+    if 0 == signal_utils.check_difference(a, bits , i)
+        print("$i good")
+    else
+        print("$i - $(signal_utils.check_difference(a, bits , i))")
     end
 end
-plot(shifted_fft)
-
-plot(shifted_fft)
-plot(tmp_fft)
-
-
-doppler_ratio = Float64[]
-posx = Float64[]
-spex = Float64[]
-accx = Float64[]
-dist = Float64[]
-for i in 1:151
-    append!(posx, sender.p[1])
-    append!(spex, sender.v[1])
-    append!(accx, sender.a[1])
-    append!(dist, abs(signal_utils.NormeVecteur(sender.p - receiver.p)))
-    append!(doppler_ratio, signal_utils.Doppler(1.0, sender, receiver))
-    Main.MyGravity.set_pos(sender, [x[i], y[i], z[i]])
-    Main.MyGravity.set_spe(sender, [x1[i], y1[i], z1[i]])
-end
-Plots.plot(x,y,z)
-Plots.plot(doppler_ratio)
-popfirst!(doppler_ratio)
+true_shift, k,  = signal_utils.ShiftSignal(fft_data, a, fft_freq)
+signal_utils.check_difference(a,k)
--- signal_generator.jl
+++ signal_generator.jl
@@ -2,7 +2,7 @@ include("./gravity_utils.jl")
 
 module signal_utils
     import Main.MyGravity
-    using Plots, FFTW, DSP
+    using Plots, FFTW, DSP, Statistics
     const global c = 2.99792458e8
     mutable struct cofdm
         "Array of boolean data transmitted"
@@ -53,10 +53,10 @@ module signal_utils
                 for i in 1:length(self.sync_channels)
                     if chan_bit == self.sync_channels[i]
                         if self.sync_data[nb_seq][i]==1           # 1 --> Higher frequency
-                            tmp_freq = doppler * (self.frequency+self.interval/2+(chan_bit-1)*self.interval)
+                            tmp_freq = doppler * (self.frequency+self.interval/2+(chan_bit-1)*self.interval+self.interval/4)
                             tmp_signal += create_sine(tmp_freq, self.sampling_rate, self.transmission_rate)
                         elseif self.sync_data[nb_seq][i]==0       # 0 --> Lower frequency
-                            tmp_freq = doppler * (self.frequency+(chan_bit-1)*self.interval)
+                            tmp_freq = doppler * (self.frequency+(chan_bit-1)*self.interval+self.interval/4)
                             tmp_signal += create_sine(tmp_freq, self.sampling_rate, self.transmission_rate)
                         end
                         sync_count += 1
@@ -65,17 +65,17 @@ module signal_utils
                 end
                 if is_done == false
                     if seq[chan_bit-sync_count]==1           # 1 --> Higher frequency
-                        tmp_freq = doppler * (self.frequency+self.interval/2+(chan_bit-1)*self.interval)
+                        tmp_freq = doppler * (self.frequency+self.interval/2+(chan_bit-1)*self.interval+self.interval/4)
                         tmp_signal += create_sine(tmp_freq, self.sampling_rate, self.transmission_rate)
                     elseif seq[chan_bit-sync_count]==0       # 0 --> Lower frequency
-                        tmp_freq = doppler * (self.frequency+(chan_bit-1)*self.interval)
+                        tmp_freq = doppler * (self.frequency+(chan_bit-1)*self.interval+self.interval/4)
                         tmp_signal += create_sine(tmp_freq, self.sampling_rate, self.transmission_rate)
                     end
                 end
             end
             # Channel filled, append the signal
             window = DSP.Windows.rect(length(tmp_signal), padding=trunc(Int, log10(self.frequency)), zerophase=false)
-            tmp_signal = tmp_signal # .* 1/self.frequency .* sin.(π/length(tmp_signal):π/length(tmp_signal):π)
+            tmp_signal = tmp_signal .* sin.(π/length(tmp_signal):π/length(tmp_signal):π)
             append!(signal, tmp_signal)
             empty!(tmp_signal)
             for i in 0:1/self.sampling_rate:self.transmission_rate #Resizing the vector for the next loop execution
@@ -142,33 +142,43 @@ module signal_utils
         sym = fft[1:trunc(Int, length(fft)/2)]
         sym_freq = freqs[1:trunc(Int, length(freqs)/2)]
         waves = Int64[]
-        around = 1
+        around = 4
         is_local_peak = false
         is_down = false
-        #Sampling bloc
-        for i in 1:trunc(Int, length(sym))-1
-            if sym[i] > sym[i+1] && !is_down   #Found peak, and value rising
-                is_down = true
-                for j in i-around:i+around
-                    if j > 0 && j < length(sym)
-                        if sym[j] > sym[i] #Local peak
-                            is_local_peak = true
-                            break
-                        end
-                        is_local_peak = false
-                    end
-                end
-                if !is_local_peak && sym[i] > 1000 # Threshold
-                    push!(waves,trunc(Int,sym_freq[i]))
-                end
-            else
-                is_down = false
+        #Peak detection algorithm
+#        for i in 1:trunc(Int, length(sym))-1
+#            if sym[i] > sym[i+1] && !is_down   #Found peak, and value rising
+#                is_down = true
+#                for j in i-around:i+around
+#                    if j > 0 && j < length(sym)
+#                        if sym[j] > sym[i] #Local peak
+#                            is_local_peak = true
+#                            break
+#                        end
+#                        is_local_peak = false
+#                    end
+#                end
+#                if !is_local_peak && sym[i] > 800 # Threshold
+#                    push!(waves,trunc(Int,sym_freq[i]))
+#                end
+#            else
+#                is_down = false
+#            end
+#        end #Peak filled
+        sig = sym
+        #Cleaning peaks
+        tmp = Int64[]
+        waves = Float64[]
+        for i in 2:length(sig)
+            if sig[i-1] == 1.0 && sig[i] == 0.0 # If we are on the end of peak
+                push!(waves, sym_freq[i])
             end
-        end #Peak filled
+        end
+        println(length(waves))
         #Bits reconstruction
         bits = Bool[]
         for channel in 1:length(waves)
-            if abs(waves[channel]-(self.frequency+(channel-1)*self.interval)) < (self.interval/2)   #If the peak is in the cofdm channel
+            if abs(waves[channel]-(self.frequency+(channel-1)*self.interval)) < (self.interval) / 2  #If the peak is in the cofdm channel
                 #0 --> False
                 push!(bits, false)
             elseif abs(waves[channel]-(self.frequency+(channel-1)*self.interval)) < (self.interval)
@@ -179,23 +189,65 @@ module signal_utils
         end
         return bits
     end
+    "Z-Score algorithm for peak detection"
+    function SmoothedZscoreAlgo(y, lag, threshold, influence)
+    # Julia implimentation of http://stackoverflow.com/a/22640362/6029703
+        n = length(y)
+        signals = zeros(n) # init signal results
+        filteredY = copy(y) # init filtered series
+        avgFilter = zeros(n) # init average filter
+        stdFilter = zeros(n) # init std filter
+        avgFilter[lag - 1] = Statistics.mean(y[1:lag]) # init first value
+        stdFilter[lag - 1] = Statistics.std(y[1:lag]) # init first value
+
+        for i in range(lag, stop=n-1)
+            if abs(y[i] - avgFilter[i-1]) > threshold*stdFilter[i-1]
+                if y[i] > avgFilter[i-1]
+                    signals[i] += 1 # postive signal
+                else
+                    signals[i] += -1 # negative signal
+                end
+                # Make influence lower
+                filteredY[i] = influence*y[i] + (1-influence)*filteredY[i-1]
+            else
+            signals[i] = 0
+            filteredY[i] = y[i]
+            end
+            avgFilter[i] = mean(filteredY[i-lag+1:i])
+            stdFilter[i] = std(filteredY[i-lag+1:i])
+        end
+        return (signals = signals, avgFilter = avgFilter, stdFilter = stdFilter)
+    end
     "This function shifts the peaks of the synchronization signal in order to calculate the Doppler effect coefficient"
     function ShiftSignal(ffts::Vector{Vector{Float64}}, self::cofdm, freq::Frequencies{Float64})
         doppler = 0.0;
         is_correct = false
         true_shift = 0.0
         true_data = [Vector{Bool}() for _ in 1:length(ffts)]
-        for shift in 0.849:0.000001:0.90   # Value for maximum shift (8Km/s)
+        zscore = [SmoothedZscoreAlgo(ffts[i], 3, 4, 0.5)[1] for i in 1:length(ffts)]
+        correl = [Plots.Plot() for _ in 1:length(ffts)]
+        for i in 1:length(ffts)
+            correl[i] = Plots.plot(freq, [zscore[i].*1000, ffts[i]], title="Bit $i", labels=["zScore" "FFT"])
+        end
+
+        for shift in 0.84:0.00001:0.86   # Value for maximum shift (8Km/s)
+            if shift == 0.85
+                println('a')
+            end
             error_number = 0
-            for bit in 1:length(ffts) # For each sequence of cofdm
+            for bit in 1:length(zscore) # For each sequence of cofdm
+                empty!(true_data[bit])
                 shifted_fft = Vector{Float64}()
                 empty!(shifted_fft)
+                if bit == 3
+                    println("wow")
+                end
                 for k in 1:length(ffts[bit])    # FFT frequency shift loop
                     if trunc(Int,k*shift) < length(ffts[bit])
                         if trunc(Int, k*shift) == 0
-                            push!(shifted_fft, ffts[bit][1])
+                            push!(shifted_fft, zscore[bit][1])
                         else
-                            push!(shifted_fft, ffts[bit][trunc(Int, k*shift)])
+                            push!(shifted_fft, zscore[bit][trunc(Int, k*shift)])
                         end
                     else
                         push!(shifted_fft, 0.0)
@@ -208,22 +260,24 @@ module signal_utils
                     for i in 1:bit  # Clear the false data
                         empty!(true_data[i])
                     end
+                    printstyled("Wrong size ($(length(bits))) with bit $bit at doppler $shift\n", color=:red)
                     break
-                elseif check_difference(self, bits) <= length(self.sync_data[bit]) # If there is some sync data with the shift
-                    printstyled("Bit $(bit-1) is shifted with $shift\n", color=:green)
-                    printstyled("The number of wrong bits in sync channels is $(check_difference(self,bits)).\n", color=:red)
-                    error_number += check_difference(self,bits)
-                    if error_number >= self.error_tolerance
+                elseif check_difference(self, bits, bit) <= length(self.sync_data[bit]) # If there is some sync data with the shift
+                    printstyled("Bit $(bit) is shifted with $shift\n", color=:green)
+                    printstyled("The number of wrong bits in sync channels is $(check_difference(self,bits, bit)).\n", color=:red)
+                    error_number += check_difference(self,bits, bit)
+                    if error_number > self.error_tolerance
                         is_correct = false
                         for i in 1:bit
                             empty(true_data[i])
                         end
-                        printstyled("Error overflow for Doppler = $shift at$bit")
+                        printstyled("Error overflow for Doppler = $shift at $bit\n$error_number errors.\n")
                         break
+                    else
+                        is_correct = true
+                        true_shift = shift
+                        append!(true_data[bit], bits)
                     end
-                    is_correct = true
-                    true_shift = shift
-                    append!(true_data[bit], bits)
                 else
                     is_correct = false
                     for i in 1:bit  # Clear the false data
@@ -232,19 +286,23 @@ module signal_utils
                     printstyled("$shift - $bit\n", color=:red)
                     break
                 end
+                if length(true_data[1]) > 11
+                    println("davai")
+                end
                 empty!(bits)
             end
             if is_correct
+                printstyled("The total number of errors in cyn channels is $error_number.\nOver a max of $(self.error_tolerance)", color=:green)
                 break
             end
         end
-        return true_shift, true_data
+        return true_shift, true_data, correl
     end
     "This function returns the number of different bit between two packets"
-    function check_difference(sync::cofdm, data::Vector{Bool})
+    function check_difference(sync::cofdm, data::Vector{Bool}, seq::Int64)
         error = 0
-        for i in 1:length(sync.sync_channel)
-            if sync.sync_data[i] != data[sync.sync_channel[i]]
+        for i in 1:length(sync.sync_channels)
+            if sync.sync_data[seq][i] != data[sync.sync_channels[i]]
                 error +=1
             end
         end
@@ -253,17 +311,29 @@ module signal_utils
     "Overload for total difference of the whole transmission"
     function check_difference(self::cofdm, data::Vector{Vector{Bool}})
         error = 0
-        if size(self.data) == size(data)
-            for i in 1:length(self.data)
-                if length(self.data[i]) == length(data[i])
-                    for j in length(self.data[i])
-                        if self.data[i][j] != data[i][j]
-                            error +=1
+        sync_count = 0
+        done = false
+            for i in 1:length(self.data) # For each sequence
+                sync_count = 0
+                for j in 1:length(self.data[i]) + length(self.sync_channels) # For each channel
+                    done = false
+                    for k in 1:length(self.sync_channels)
+                        if j == self.sync_channels[k] && !done
+                            if self.sync_data[i][k] != data[i][j]
+                                error +=1
+                            end
+                            sync_count += 1
+                            done = true
+                            break
+                        end
+                    end
+                    if !done
+                        if self.data[i][j-sync_count] != data[i][j]
+                            error += 1
                         end
                     end
                 end
             end
-        end
         return error
     end
 end
